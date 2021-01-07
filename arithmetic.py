994197136a5ab4d3bf8f8dce9f5259ab4d3979b6
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-01-07 00:16:16 +0000

    Complete Math Functions

--- arithmetic.py
+++ arithmetic.py
@@ -1,35 +1,68 @@
 """Functions for common math operations."""
+# sudo code:
 
+# in function "add", fix the return value. 
+    # change the return value to first number plus second number 
+
+# in function "subtract", add the return value
+    # the return value should be first number minus second number
+
+# in function "multiply", add return value
+    #the return value should be first number multiplied by the second number
+
+# in function "divide", add return value
+    #the return value should be first number divied by the second number
+
+# in function "square", add return value
+    # the return value should be number multiplied by the number
+    
+# in function "cube", add return value
+    # the return value should be number to the third power
+
+# in function "power", add return value
+    # the return value should be the first number to the second number power
+
+#in function "mod", add return value
+    #return value should be the remainder from first number divided second number
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
 
-    return 10
+    return num1 + num2
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
+    return num1 - num2
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
-
+    return num1 * num2
+    
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
+    return num1 / num2
 
 def square(num1):
     """Return the square of the input."""
 
+    return num1 ** 2
 
 def cube(num1):
     """Return the cube of the input."""
+
+    return num1 ** 3
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    return num1 ** num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+
+    return num1 % num2
